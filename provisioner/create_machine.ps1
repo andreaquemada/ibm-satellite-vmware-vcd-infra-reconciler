2c5b4010af1fe739dcabe352e651b59c049d2e8b
krglosse <krglosse@us.ibm.com>
2023-05-18 16:01:28 -0500

    updates

--- provisioner/create_machine.ps1
+++ provisioner/create_machine.ps1
@@ -1,5 +1,8 @@
+# vSphere command index: https://developer.vmware.com/docs/powercli/latest/products/vmwarevsphereandvsan/commands-index/
 for($i = 0; $i -le 10; $i++)
 {
+ # will need to evaluate how we connect. May need to use vSphere commands: Connect-VIServer
+ # https://developer.vmware.com/docs/powercli/latest/products/vmwarevsphereandvsan/
  Connect-CIServer -Server "$Env:VCD_SERVER" -Org "$Env:VCD_ORG" -User "$Env:VCD_USER" -Pass "$Env:VCD_PASSWORD"
  if ( $?)
  {
@@ -10,6 +13,7 @@ for($i = 0; $i -le 10; $i++)
 for($i = 0; $i -le 10; $i++)
 {
  $Error.Clear()
+ # New-VApp https://developer.vmware.com/docs/powercli/latest/vmware.vimautomation.core/commands/new-vapp/#new
  New-CIVApp -Name "$Env:VAPP_NAME" -OrgVdc "$Env:VCD_ORG_VDC" -VAppTemplate "rhcos OpenShift 4.10.16" -StorageLease $null -RuntimeLease $null
  if ( $?)
  {
@@ -25,6 +29,7 @@ for($i = 0; $i -le 10; $i++)
 for($i = 0; $i -le 10; $i++)
 {
  $Error.Clear()
+ # TODO
  New-CIVAppNetwork -Direct -ParentOrgVdcNetwork "$Env:VCD_NETWORK" -Vapp "$Env:VAPP_NAME"
  if ( $?)
  {
@@ -37,9 +42,11 @@ for($i = 0; $i -le 10; $i++)
  }
  Start-Sleep -Seconds 60
 }
+# TODO
 Remove-CIVAppNetwork -VappNetwork "VM Network" -Confirm:$false
 for($i = 0; $i -le 10; $i++)
 {
+ # Get-VApp
  $vm = Get-CIVApp -OrgVdc "$Env:VCD_ORG_VDC" -Name "$Env:VAPP_NAME" | Get-CIVM
  if ( $?)
  {
@@ -88,6 +95,7 @@ for($i = 0; $i -le 10; $i++)
  $initarray2 = @( $entry2 )
  $vsa.Items[0].OvfPropertyConfigurationValue = $initarray1
  $vsa.Items[1].OvfPropertyConfigurationValue = $initarray2
+ # TODO: do not pass in ignition data. We will pass the RHEL attach script
  $ignitionData = Get-Content -path "$Env:IGN_FILE_PATH_WITH_HOSTNAME_64"
  $vsa.Items[0].OvfPropertyConfigurationValue[0].Value = $ignitionData
  $vsa.Items[1].OvfPropertyConfigurationValue[0].Value = "base64"
@@ -100,6 +108,7 @@ for($i = 0; $i -le 10; $i++)
 }
 for($i = 0; $i -le 10; $i++)
 {
+ # TODO
  $myVappNetwork2 = Get-CIVAppNetwork -Name "$Env:VCD_NETWORK"  -VApp "$Env:VAPP_NAME"
  Get-CIVApp -OrgVdc "$Env:VCD_ORG_VDC" -Name "$Env:VAPP_NAME"  | Get-CIVM | Get-CINetworkAdapter | Set-CINetworkAdapter -Connected $true -IPAddressAllocationMode Dhcp  -Primary -VAppNetwork $myVappNetwork2
  if ( $?)
@@ -110,6 +119,7 @@ for($i = 0; $i -le 10; $i++)
 }
 for($i = 0; $i -le 10; $i++)
 {
+ # Set-VDisk
  Update-CIVMDiskSize -VM $vm -BusType paravirtual -BusId 0 -UnitID 0 -NewDiskSize "$ENV:DISK_1_SIZE" -TaskTimeout 600
  if ( $?)
  {
@@ -121,6 +131,7 @@ $diskSizeItr = "$ENV:DISK_2_SIZE"
 if (-Not (("$diskSizeItr" -eq $null) -or ("$diskSizeItr" -eq ""))){
  for($i = 0; $i -le 10; $i++)
  {
+  # New-VDisk
   Add-CIVMDisk -VM $vm -BusType paravirtual -BusId 0 -UnitID 1 -DiskSize "$diskSizeItr" -TaskTimeout 600
   if ( $?)
   {
@@ -133,6 +144,7 @@ $diskSizeItr = "$ENV:DISK_3_SIZE"
 if (-Not (("$diskSizeItr" -eq $null) -or ("$diskSizeItr" -eq ""))){
  for($i = 0; $i -le 10; $i++)
  {
+  # New-VDisk
   Add-CIVMDisk -VM $vm -BusType paravirtual -BusId 0 -UnitID 2 -DiskSize "$diskSizeItr" -TaskTimeout 600
   if ( $?)
   {
@@ -145,6 +157,7 @@ $diskSizeItr = "$ENV:DISK_4_SIZE"
 if (-Not (("$diskSizeItr" -eq $null) -or ("$diskSizeItr" -eq ""))){
  for($i = 0; $i -le 10; $i++)
  {
+  # New-VDisk
   Add-CIVMDisk -VM $vm -BusType paravirtual -BusId 0 -UnitID 3 -DiskSize "$diskSizeItr" -TaskTimeout 600
   if ( $?)
   {
@@ -157,6 +170,7 @@ $diskSizeItr = "$ENV:DISK_5_SIZE"
 if (-Not (("$diskSizeItr" -eq $null) -or ("$diskSizeItr" -eq ""))){
  for($i = 0; $i -le 10; $i++)
  {
+  # New-VDisk
   Add-CIVMDisk -VM $vm -BusType paravirtual -BusId 0 -UnitID 4 -DiskSize "$diskSizeItr" -TaskTimeout 600
   if ( $?)
   {
@@ -167,6 +181,7 @@ if (-Not (("$diskSizeItr" -eq $null) -or ("$diskSizeItr" -eq ""))){
 }
 for($i = 0; $i -le 10; $i++)
 {
+ # Start-VIApplianceService
  Start-CIVApp -VApp "$Env:VAPP_NAME"
  if ( $?)
  {
